2c9cb0d99e61560b527ab264fed1e4fa3717454d
Tom <13634684+dirtyformal@users.noreply.github.com>
2024-03-31 14:39:25 +1300

    refactor: Generated pair character exclusion

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
 const validChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'P', 'Q', 'R', 'S'];
 
-function getValidCharPair() {
-  let localArray = validChars;
+function getValidCharPair(excludeChars = []) {
+  let localArray = validChars.filter(char => !excludeChars.includes(char));
   let workingArray = [];
 
   for (let i = 0; i < 2; i++) {
-    // Calculate random index
-    const randomIndex = Math.floor(Math.random() * localArray.length);  
-
-    // If the first randomIndex value points to an 'S', restart the for loop.
-    if (randomIndex === localArray.length){
-      i=-1; continue;
-    }
-
-
+    const randomIndex = Math.floor(Math.random() * localArray.length);
     const element = localArray[randomIndex];
-    localArray = localArray.slice(randomIndex + 1);
-
+    localArray.splice(randomIndex, 1);
     workingArray.push(element);
-
   }
 
-  return workingArray;
+  return workingArray.sort();
 }
 
 export function getSingleSelcal() {
   let res = [];
 
   const firstPair = getValidCharPair();
-  const secondPair = getValidCharPair();
-
-  // Iterate through the second pair to check if any of the characters are the same as the first pair, if so, regenerate the second pair.
-  for (let i = 0; i < secondPair.length; i++) {
-    if (firstPair.includes(secondPair[i])) {
-      return getSingleSelcal();
-    }
-  }
-  
+  const secondPair = getValidCharPair(firstPair);
 
   res.push(firstPair.join(''));
   res.push(secondPair.join(''));
